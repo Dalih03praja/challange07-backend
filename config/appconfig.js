02d1930916b9e43ec7eba50a173cf1d0bb813b2f
(Dalih) <dalih03praja@gail.com>
2022-06-03 09:12:22 +0700

    heroku

--- config/appconfig.js
+++ config/appconfig.js
@@ -5,6 +5,8 @@
 
 require("dotenv").config();
 
+const {DB_USERNAME, DB_PASSWORD, DB_HOST, DB_NAME} = process.env;
+
 module.exports = {
   app: {
     port: process.env.DEV_APP_PORT || 8000,
@@ -13,5 +15,29 @@ module.exports = {
     server: process.env.SERVER || "localhost",
     jwtSecret: process.env.SECRET_KEY || "BinarUniqueKey",
     jwtExpire: process.env.JWT_EXPIRE || "12h",
-  }
+  },
+development: {
+    username: DB_USERNAME,
+    password: DB_PASSWORD,
+    database: DB_NAME,
+    host: DB_HOST,
+    dialect: "postgres",
+    ssl: true,
+  },
+test: {
+    username: DB_USERNAME,
+    password: DB_PASSWORD,
+    database: DB_NAME,
+    host: DB_HOST,
+    dialect: "postgres",
+    ssl: true,
+  },
+production: {
+    username: DB_USERNAME,
+    password: DB_PASSWORD,
+    database: DB_NAME,
+    host: DB_HOST,
+    dialect: "postgres",
+    ssl: true,
+  },
 };
